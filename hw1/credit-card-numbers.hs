a4a1af6258a8fbbd8c7ed966c092b4a3d4205afd
Bruno Leite Alves <brunoleitealves@gmail.com>
2016-12-15 22:15:16 -0200

    Adds logic for extract digits

--- hw1/credit-card-numbers.hs
+++ hw1/credit-card-numbers.hs
@@ -1,4 +1,8 @@
 toDigits :: Integer -> [Integer]
+
 toDigits n
-  | n < 0     = []
-  | otherwise = n:[]
+  | n <= 0     = []
+  | n >= 1000  = n `div` 1000:toDigits(n `mod` 1000)
+  | n >= 100   = n `div` 100:toDigits(n `mod` 100)
+  | n >= 10    = n `div` 10:toDigits(n `mod` 10)
+  | otherwise  = [n]
