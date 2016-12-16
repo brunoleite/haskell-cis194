79b0925f0c330f9c69a277768aa4a8bbb93a230e
Bruno Leite Alves <brunoleitealves@gmail.com>
2016-12-16 18:26:47 -0200

    Adds reverse to digits

--- hw1/credit-card-numbers.hs
+++ hw1/credit-card-numbers.hs
@@ -1,8 +1,11 @@
 toDigits :: Integer -> [Integer]
+toDigitsReverse :: Integer -> [Integer]
 
 toDigits n
-  | n <= 0     = []
-  | n >= 1000  = n `div` 1000:toDigits(n `mod` 1000)
-  | n >= 100   = n `div` 100:toDigits(n `mod` 100)
-  | n >= 10    = n `div` 10:toDigits(n `mod` 10)
-  | otherwise  = [n]
+  | n <= 0    = []
+  | n >= 1000 = n `div` 1000:toDigits(n `mod` 1000)
+  | n >= 100  = n `div` 100:toDigits(n `mod` 100)
+  | n >= 10   = n `div` 10:toDigits(n `mod` 10)
+  | otherwise = [n]
+
+toDigitsReverse n = reverse (toDigits(n))
